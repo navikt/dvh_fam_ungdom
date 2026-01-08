7b0a664e8b9d49c91c7d1a169a355faf9f5d4c56
nav-gard <gard.sigurd.troim@nav.no>
2026-01-08 12:43:39 +0000

    Test

--- models/UP_utpakking/up_meldinger_til_aa_pakke_ut.sql
+++ models/UP_utpakking/up_meldinger_til_aa_pakke_ut.sql
@@ -6,7 +6,7 @@
 
 with ungdomsprogram_meta_data as (
   select pk_up_meta_data, kafka_offset, kafka_mottatt_dato, kafka_topic, kafka_partition, melding from {{ source ('fam_ungdom', 'fam_up_meta_data') }}
-    where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
+    where kafka_mottatt_dato >= sysdate - 60 and kafka_offset not in (
       select kafka_offset from {{ source ('fam_ungdom', 'fam_up_fagsak') }})
 )
 
