7762d83448b9f708dfc3766b4837331e8cba43ff
AArafaR <97885051+AArafaR@users.noreply.github.com>
2026-03-19 14:13:09 +0100

    la til database i profiles.yml og endret dbt_run.py

--- airflow/dbt_run.py
+++ airflow/dbt_run.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import requests
 import time
 import json
 import sys
@@ -7,6 +8,9 @@ import logging
 from typing import List
 from google.cloud import secretmanager
 import shlex
+
+
+DBT_PROJECT = 'dvh_fam_bb'
 
 def set_secrets_as_envs():
   secrets = secretmanager.SecretManagerServiceClient()
@@ -29,19 +33,43 @@ def filter_logs(file_path: str) -> List[dict]:
         logs.append(json.loads(log))
 
     dbt_codes = [
+      'Q007', #PASS
       'Q009', #PASS
       'Q010', #WARN
-      'Q011', #FAIL
+      #'Q011', #RUN not of interest
+      'Q018', #Freshness PASS
       'Q019', #Freshness WARN
       'Q020', #Freshness PASS
       'Z021', #Info about warning in tests
       'Z022', #Info about failing tests
       'E040', #Total runtime
+      'Q035', #Nothing to do
     ]
 
-    filtered_logs = [log for log in logs if log['info']['code'] in dbt_codes]
+    filtered_logs = []
+    for log in logs:
+       if log.get("info") and log["info"]["code"] in dbt_codes:
+          filtered_logs.append(log["info"])
 
     return filtered_logs
+
+def publish_docs(my_path):
+    # Connection informasjon for å pushe dbt docs
+    dbt_docs_url = f'{os.environ["DBT_DOCS_URL"]}{DBT_PROJECT}'
+    files = [
+        my_path + "/target/manifest.json",
+        my_path + "/target/catalog.json",
+        my_path + "/target/index.html",
+    ]
+    multipart_form_data = {}
+    for file_path in files:
+        file_name = os.path.basename(file_path)
+        with open(file_path, "rb") as file:
+            file_contents = file.read()
+            multipart_form_data[file_name] = (file_name, file_contents)
+
+    res = requests.put(dbt_docs_url, files=multipart_form_data)
+    res.raise_for_status()
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
@@ -73,19 +101,30 @@ if __name__ == "__main__":
     project_path = os.path.dirname(os.getcwd())
     logger.info(f"Prosjekt path er: {project_path}")
 
-    try:
-        process_deps = subprocess.run(['dbt', 'deps'],
-            check=True, capture_output=True)
-        output = subprocess.run(
-            (
-              ["dbt", "--no-use-colors", "--log-format", "json"] +
-              command +
-              ["--profiles-dir", profiles_dir, "--project-dir", project_path]
-            ),
-            check=True, capture_output=True
-        )
-        logger.info(output.stdout.decode("utf-8"))
-        logger.debug(dbt_logg(project_path))
-    except subprocess.CalledProcessError as err:
-        raise Exception(logger.error(dbt_logg(project_path)),
-                       err.stdout.decode("utf-8"))
+    def run_dbt(command: List[str]):
+            try:
+                logger.debug(f"running command: {command}")
+                output = subprocess.run(
+                    (
+                      ["dbt", "--no-use-colors", "--log-format", "json"] +
+                      command +
+                      ["--profiles-dir", profiles_dir, "--project-dir", project_path]
+                    ),
+                    check=True, capture_output=True
+                )
+                if "docs" in command:
+                  publish_docs(project_path)
+                logger.info(output.stdout.decode("utf-8"))
+                logger.debug(dbt_logg(project_path))
+            except subprocess.CalledProcessError as err:
+                raise Exception(logger.error(dbt_logg(project_path)),
+                                err.stdout.decode("utf-8"))
+
+
+    run_dbt(["deps"])
+    #run_dbt(["compile"])
+    run_dbt(command)
+
+    filtered_logs = filter_logs(f"{project_path}/logs/dbt.log")
+    write_to_xcom_push_file(filtered_logs)
+    
