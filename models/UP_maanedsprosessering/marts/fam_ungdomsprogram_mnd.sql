1fb3030ba015c368ce773cdabad3998a5f94c1e2
nav-gard <gard.sigurd.troim@nav.no>
2026-04-17 15:39:50 +0000

    La til FK_DIM_SAK_YTELSE, FORSTE_SOKNADSDATO, BEHANDLINGSPERIODE_FOM og BEHANDLINGSPERIODE_TOM. Oppdatert profiles.yml

--- models/UP_maanedsprosessering/marts/fam_ungdomsprogram_mnd.sql
+++ models/UP_maanedsprosessering/marts/fam_ungdomsprogram_mnd.sql
@@ -14,21 +14,26 @@ with fact as (
         ,SISTE.PROGRAMDELTAKELSE_TOM_NY 
         ,SISTE.FK_UP_FAGSAK 
         ,SISTE.BEHANDLINGSPERIODER_TOM_MAX
+        ,SISTE.YTELSE_TYPE
         ,SISTE.STAT_AARMND 
         ,SISTE.STAT_AARMND_DT
         ,SISTE.ANTALL_DAGER
-        ,SISTE.UTBET_FOM
-        ,SISTE.UTBET_TOM
+        --,SISTE.UTBET_FOM -- Nevnt flere ganger etter Arafa sin endring, burde rydde opp i dette senere
+        --,SISTE.UTBET_TOM
         --,SISTE.DATO
         ,SISTE.SISTE_DATO_I_PERIODEN
         ,SISTE.UTFALL
+        ,SISTE.BEHANDLINGSPERIODE_TOM
+        ,SISTE.BEHANDLINGSPERIODE_FOM
         ,SISTE.ANTALL_BARN
         ,SISTE.SATSPERIODE_FOM
         ,SISTE.SATSPERIODE_TOM
         ,SISTE.dagsats_barnetillegg
         ,SISTE.DAGSATS_UTEN_BARNETILLEGG
+        ,SISTE.SATS_TYPE
         ,SISTE.FK_PERSON1_MOTTAKER
         ,SISTE.VEDTAKSTIDSPUNKT
+        ,SISTE.FORSTE_SOKNADSDATO 
         --,SISTE.DAGSATS_TEMP
         ,SISTE.BUDSJETT
         ,SISTE.REDUKSJON
@@ -40,20 +45,21 @@ with fact as (
         ,SISTE.BOSTED_LAND
         ,SISTE.Alder
         ,CASE
-          WHEN SISTE.Alder < 25 THEN 'LAV' 
-          Else 'HØY'
-          END SATS_TYPE_KALK
+            WHEN SISTE.Alder < 25 THEN 'LAV' 
+            Else 'HØY'
+            END SATS_TYPE_KALK
         ,SISTE.FYLKE_NR
         ,SISTE.FYLKE_NAVN
         ,SISTE.INNTEKT
-        ,SISTE.YTELSE_TYPE
-        ,SISTE.SATS_TYPE
+        ,SISTE.UTBET_FOM
+        ,SISTE.UTBET_TOM
         ,SISTE.FK_DIM_ALDER
+        ,SISTE.FK_DIM_SAK_YTELSE
         ,{{ var("gyldig_flagg") }} GYLDIG_FLAGG
         ,TO_DATE('{{ var("max_vedtaksdato") }}', 'yyyymmdd') MAX_VEDTAKSDATO
         ,'{{ var("periode_type") }}' PERIODE_TYPE
         ,localtimestamp AS lastet_dato
-    from {{ ref('int_up_join_inntekt') }} SISTE
+    from {{ ref('int_up_join_dim_sak_ytelse') }} SISTE
 )
 
 select
@@ -64,28 +70,31 @@ select
     ,PROGRAMDELTAKELSE_TOM_NY PROGRAMDELTAKELSE_TOM_KALK
     ,FK_UP_FAGSAK 
     ,BEHANDLINGSPERIODER_TOM_MAX
+    ,YTELSE_TYPE
     ,STAT_AARMND 
     ,STAT_AARMND_DT
     ,ANTALL_DAGER
-    ,UTBET_FOM
-    ,UTBET_TOM
+    --,UTBET_FOM
+    --,UTBET_TOM
     --,DATO
     ,SISTE_DATO_I_PERIODEN
     ,UTFALL
+    ,BEHANDLINGSPERIODE_TOM
+    ,BEHANDLINGSPERIODE_FOM
     ,ANTALL_BARN
     ,SATSPERIODE_FOM
     ,SATSPERIODE_TOM
     ,dagsats_barnetillegg
     ,DAGSATS_UTEN_BARNETILLEGG
+    ,SATS_TYPE
     ,FK_PERSON1_MOTTAKER
     ,VEDTAKSTIDSPUNKT
+    ,FORSTE_SOKNADSDATO 
     --,DAGSATS_TEMP
     ,BUDSJETT
     ,REDUKSJON
     ,BELOP
-    ,FK_DIM_ALDER
     ,'UP_VEDTAK' KILDESYSTEM
-    ,YTELSE_TYPE
     ,DAGSATS
     ,FK_DIM_PERSON_MOTTAKER
     ,FK_DIM_GEOGRAFI_BOSTED
@@ -96,7 +105,10 @@ select
     ,FYLKE_NR
     ,FYLKE_NAVN
     ,INNTEKT
-    ,SATS_TYPE
+    ,UTBET_FOM
+    ,UTBET_TOM
+    ,FK_DIM_ALDER
+    ,FK_DIM_SAK_YTELSE
     ,GYLDIG_FLAGG
     ,max_vedtaksdato
     ,PERIODE_TYPE
